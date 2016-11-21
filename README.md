42437e4a5aa24eba17c7d595668c9317ec50fe15
Lee Solomon <leesolomon@Lees-MacBook-Pro.local>
2016-11-21 14:19:44 -0600

    initial_commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Simple Image Display for TDI Challenge by Michelle Harris
+
+Allows a user to upload an image and have the image displayed back to them.
---
+++ app.py
@@ -0,0 +1,41 @@
+import os
+from flask import Flask, request, redirect, url_for, send_from_directory, render_template
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'static/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def index():
+    return render_template('index.html')           
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Get the name of the uploaded file
+    file = request.files['file']
+    # Check if the file is one of the allowed types/extensions
+    if file and allowed_file(file.filename):
+        # Make the filename safe, remove unsupported chars
+        filename = secure_filename(file.filename)
+        # Move the file form the temporal folder to the static image folder
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # Redirect the user to the uploaded_file route to view the image
+        return redirect(url_for('uploaded_file', filename=filename))
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+	#Loads the image from where it was saved
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+	
+
+if __name__ == '__main__':
+  app.run(port=33507)
